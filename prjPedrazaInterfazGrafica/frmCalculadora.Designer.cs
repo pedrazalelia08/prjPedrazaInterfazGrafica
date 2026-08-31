0ed5e7532cb7c8b223bd060dc6b3a53ce94a71de
Lelia Pedraza <pedrazalelia1@gmail.com>
2026-08-31 11:25:58 -0300

    31/8

--- prjPedrazaInterfazGrafica/frmCalculadora.Designer.cs
+++ prjPedrazaInterfazGrafica/frmCalculadora.Designer.cs
@@ -28,33 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculadora));
             label2 = new Label();
             label3 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
+            txtNumero1 = new TextBox();
+            txtNumero2 = new TextBox();
+            btnSuma = new Button();
+            btnResta = new Button();
+            btnMultiplicación = new Button();
+            btnDivisión = new Button();
             label4 = new Label();
-            textBox3 = new TextBox();
+            lblTotal = new TextBox();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(107, 18);
-            label1.Name = "label1";
-            label1.Size = new Size(91, 15);
-            label1.TabIndex = 0;
-            label1.Text = "CALCULADORA";
-            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(26, 57);
+            label2.BackColor = Color.Thistle;
+            label2.Location = new Point(31, 34);
             label2.Name = "label2";
             label2.Size = new Size(63, 15);
             label2.TabIndex = 1;
@@ -63,95 +54,107 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(26, 98);
+            label3.BackColor = Color.Thistle;
+            label3.Location = new Point(31, 85);
             label3.Name = "label3";
             label3.Size = new Size(63, 15);
             label3.TabIndex = 2;
             label3.Text = "Numero 2:";
+            label3.Click += label3_Click;
             // 
-            // textBox1
+            // txtNumero1
             // 
-            textBox1.Location = new Point(107, 53);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 3;
+            txtNumero1.Location = new Point(107, 34);
+            txtNumero1.Name = "txtNumero1";
+            txtNumero1.Size = new Size(100, 23);
+            txtNumero1.TabIndex = 3;
+            txtNumero1.TextChanged += txtNumero1_TextChanged;
             // 
-            // textBox2
+            // txtNumero2
             // 
-            textBox2.Location = new Point(107, 94);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 4;
+            txtNumero2.Location = new Point(107, 82);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 4;
             // 
-            // button1
+            // btnSuma
             // 
-            button1.Location = new Point(31, 143);
-            button1.Name = "button1";
-            button1.Size = new Size(45, 27);
-            button1.TabIndex = 5;
-            button1.Text = "+";
-            button1.UseVisualStyleBackColor = true;
+            btnSuma.BackColor = Color.PaleVioletRed;
+            btnSuma.Location = new Point(39, 127);
+            btnSuma.Name = "btnSuma";
+            btnSuma.Size = new Size(45, 27);
+            btnSuma.TabIndex = 5;
+            btnSuma.Text = "+";
+            btnSuma.UseVisualStyleBackColor = false;
+            btnSuma.Click += btnSuma_Click;
             // 
-            // button2
+            // btnResta
             // 
-            button2.Location = new Point(90, 143);
-            button2.Name = "button2";
-            button2.Size = new Size(45, 28);
-            button2.TabIndex = 6;
-            button2.Text = "-";
-            button2.UseVisualStyleBackColor = true;
+            btnResta.BackColor = Color.PaleVioletRed;
+            btnResta.Location = new Point(99, 127);
+            btnResta.Name = "btnResta";
+            btnResta.Size = new Size(45, 28);
+            btnResta.TabIndex = 6;
+            btnResta.Text = "-";
+            btnResta.UseVisualStyleBackColor = false;
+            btnResta.Click += btnResta_Click;
             // 
-            // button3
+            // btnMultiplicación
             // 
-            button3.Location = new Point(150, 143);
-            button3.Name = "button3";
-            button3.Size = new Size(44, 27);
-            button3.TabIndex = 7;
-            button3.Text = "x";
-            button3.UseVisualStyleBackColor = true;
+            btnMultiplicación.BackColor = Color.PaleVioletRed;
+            btnMultiplicación.Location = new Point(159, 128);
+            btnMultiplicación.Name = "btnMultiplicación";
+            btnMultiplicación.Size = new Size(44, 27);
+            btnMultiplicación.TabIndex = 7;
+            btnMultiplicación.Text = "x";
+            btnMultiplicación.UseVisualStyleBackColor = false;
+            btnMultiplicación.Click += btnMultiplicación_Click;
             // 
-            // button4
+            // btnDivisión
             // 
-            button4.Location = new Point(209, 143);
-            button4.Name = "button4";
-            button4.Size = new Size(45, 27);
-            button4.TabIndex = 8;
-            button4.Text = "/";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += button4_Click;
+            btnDivisión.BackColor = Color.PaleVioletRed;
+            btnDivisión.Location = new Point(219, 127);
+            btnDivisión.Name = "btnDivisión";
+            btnDivisión.Size = new Size(45, 27);
+            btnDivisión.TabIndex = 8;
+            btnDivisión.Text = "÷";
+            btnDivisión.UseVisualStyleBackColor = false;
+            btnDivisión.Click += button4_Click;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(41, 195);
+            label4.BackColor = Color.Plum;
+            label4.Location = new Point(58, 194);
             label4.Name = "label4";
             label4.Size = new Size(62, 15);
             label4.TabIndex = 9;
             label4.Text = "Resultado:";
             // 
-            // textBox3
+            // lblTotal
             // 
-            textBox3.Location = new Point(126, 191);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
-            textBox3.TabIndex = 10;
+            lblTotal.Location = new Point(126, 191);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(100, 23);
+            lblTotal.TabIndex = 10;
             // 
             // frmCalculadora
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(308, 246);
-            Controls.Add(textBox3);
+            BackColor = Color.MistyRose;
+            ClientSize = new Size(286, 233);
+            Controls.Add(lblTotal);
             Controls.Add(label4);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(btnDivisión);
+            Controls.Add(btnMultiplicación);
+            Controls.Add(btnResta);
+            Controls.Add(btnSuma);
+            Controls.Add(txtNumero2);
+            Controls.Add(txtNumero1);
             Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "frmCalculadora";
             Text = "frmCalculadora";
             ResumeLayout(false);
@@ -159,17 +162,15 @@
         }
 
         #endregion
-
-        private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private TextBox txtNumero1;
+        private TextBox txtNumero2;
+        private Button btnSuma;
+        private Button btnResta;
+        private Button btnMultiplicación;
+        private Button btnDivisión;
         private Label label4;
-        private TextBox textBox3;
+        private TextBox lblTotal;
     }
 }
--- prjPedrazaInterfazGrafica/frmCalculadora.cs
+++ prjPedrazaInterfazGrafica/frmCalculadora.cs
@@ -24,5 +24,52 @@ namespace prjPedrazaInterfazGrafica
         {
 
         }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            double numero1 = double.Parse(txtNumero1.Text);
+            double numero2 = double.Parse(txtNumero2.Text);
+
+            //calculo suma
+            double calculo = numero1 + numero2;
+
+            lblTotal.Text = calculo.ToString();
+
+
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            double numero1 = double.Parse(txtNumero1.Text);
+            double numero2 = double.Parse(txtNumero2.Text);
+
+            //Calculo resta
+            double calculo = numero1 - numero2;
+
+            lblTotal.Text = calculo.ToString();
+
+        }
+
+        private void btnMultiplicación_Click(object sender, EventArgs e)
+        {
+            double numero1 = double.Parse(txtNumero1.Text);
+            double numero2 = double.Parse(txtNumero2.Text);
+
+            //calculo multiplicación
+            double calculo = numero1 * numero2;
+
+            lblTotal.Text = calculo.ToString();
+
+        }
     }
 }
